c58c4edaad68cb6ac506f0756b95c22167ec26a3
ryzecous <r3j9c89@gmail.com>
2021-04-13 14:18:57 -0400

    created book.js model / created bookController.js and structured out functions

---
+++ controllers/bookController.js
@@ -0,0 +1,21 @@
+const Book = require('../models/book')
+
+module.exports = {
+    getBooks: function(req, res) {
+
+    },
+
+    creatBooks: function(req, res) {
+
+    },
+
+    getBook: function(req, res) {
+
+    },
+
+    deleteBook: function(req, res) {
+
+    }
+
+
+}
